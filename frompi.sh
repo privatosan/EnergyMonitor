4ef4c3a5ec4d72b6424183cf83bd137187d4211a
Andreas Heumann <andreasheu@gmail.com>
2020-12-27 09:39:28 +0000

    Add JSON parser nlohmann/json. Fix topi and frompi scripts.

--- frompi.sh
+++ frompi.sh
@@ -1,1 +1,1 @@
-scp -p -r pi@192.168.178.21:/var/opt/emoncms/projects/EnergyMonitor .
+scp -p -r pi@192.168.178.21:/var/opt/emoncms/projects/EnergyMonitor/* .
--- topi.sh
+++ topi.sh
@@ -1,2 +1,2 @@
-scp -p -r CMakeLists.txt src/ ext/ pi@192.168.178.25:/home/pi/data/energymeter/
-#scp -p CMakeLists.txt pi@192.168.178.25:/home/pi/data/energymeter/
+scp -p -r CMakeLists.txt src/ ext/ pi@192.168.178.12:/var/opt/emoncms/projects/EnergyMonitor/
+#scp -p CMakeLists.txt pi@192.168.178.12:/var/opt/emoncms/projects/EnergyMonitor/
